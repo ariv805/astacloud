42a5a96472c408ead3d7ca26acc487d077290064
ariv805 <vitovian5@gmail.com>
2026-01-29 21:27:37 +0700

    Fix ID for Temperglass item in script.js

--- script.js
+++ script.js
@@ -179,15 +179,16 @@ const dataManager = {
                 discount: 0,
                 image: "https://image2url.com/r2/default/images/1769590602521-c4f821d2-36f8-4147-98be-fb61ed011e17.jpg",
                 description: "Webcam HD 1080p untuk video conference dan streaming"
-            }
+            },
             {
-                id: 6,
+                id: 5,
                 name: "Temperglass",
                 price: "Rp 10.000 - 15.000",
                 discount: 0,
                 image: "https://image2url.com/r2/default/images/1769696487732-ec9aa4d4-579b-42cf-b699-d633edee93e2.jpg",
                 description: "Anti gores & benturan, jernih, sentuhan tetap halus."
-             }
+            },
+            
         ];
     },
     
